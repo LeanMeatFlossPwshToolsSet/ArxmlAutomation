48b2e8a7e70dca1d00ee580afde82baeb47f3629
Hansong Li <li-hans@outlook.com>
2022-08-01 10:51:54 +0800

    Add action to publish powershell modules

---
+++ PSActions/PSGalleryPublish.ps1
@@ -0,0 +1,18 @@
+param(
+    [string]
+    $NugetKey
+)
+$PSVersionTable
+dir env:
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Get-ChildItem -Path "$($env:GITHUB_WORKSPACE)/ArxmlAutomation" -Directory |ForEach-Object{
+    if($env:GITHUB_REF_NAME -eq "main"){
+        # main branch methods
+        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -Verbose -Confirm -Force
+    }
+    else {
+        # sub branch methods
+        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -WhatIf -Verbose -Confirm -Force
+    }
+}
+
