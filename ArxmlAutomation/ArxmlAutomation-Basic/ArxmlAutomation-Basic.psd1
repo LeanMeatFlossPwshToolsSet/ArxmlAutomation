71a591396defd4c280c512b0c736256d050b74e4
Hansong Li <li-hans@outlook.com>
2022-08-01 12:30:18 +0800

    change directory  structure

--- ArxmlAutomation/ArxmlAutomation-Basic/ArxmlAutomation-Basic.psd1
+++ ArxmlAutomation/ArxmlAutomation-Basic/ArxmlAutomation-Basic.psd1
@@ -9,7 +9,7 @@
 @{
 
 # Script module or binary module file associated with this manifest.
-RootModule = '../PSModules/ArxmlAutomation-Basic.psm1'
+RootModule = 'ArxmlAutomation-Basic.psm1'
 
 # Version number of this module.
 ModuleVersion = '0.0.1'
@@ -74,7 +74,7 @@ Description = 'The basic module to perform arxml automation on powershell'
 # RequiredModules = @()
 
 # Assemblies that must be loaded prior to importing this module
-RequiredAssemblies = @("../Library/AutosarCollection.dll")
+RequiredAssemblies = @("Library/AutosarCollection.dll")
 
 # Script files (.ps1) that are run in the caller's environment prior to importing this module.
 # ScriptsToProcess = @()
--- PSActions/PSGalleryPublish.ps1
+++ PSActions/PSGalleryPublish.ps1
@@ -5,12 +5,14 @@ param(
 $PSVersionTable
 dir env:
 Set-PSRepository PSGallery -InstallationPolicy Trusted
-Get-ChildItem -Path "$($env:GITHUB_WORKSPACE)/PSModulesManifest"
-if($env:GITHUB_REF_NAME -eq "main"){
-    # main branch methods
-    Publish-Module -Path "$($env:GITHUB_WORKSPACE)/PSModulesManifest" -NuGetApiKey $NugetKey -Verbose -Confirm
+Get-ChildItem -Path "$($env:GITHUB_WORKSPACE)/ArxmlAutomation" -Directory |ForEach-Object{
+    if($env:GITHUB_REF_NAME -eq "main"){
+        # main branch methods
+        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -Verbose -Confirm -WhatIf
+    }
+    else {
+        # sub branch methods
+        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -WhatIf -Verbose -Confirm
+    }
 }
-else {
-    # sub branch methods
-    Publish-Module -Path "$($env:GITHUB_WORKSPACE)/PSModulesManifest" -NuGetApiKey $NugetKey -WhatIf -Verbose -Confirm
-}
+
