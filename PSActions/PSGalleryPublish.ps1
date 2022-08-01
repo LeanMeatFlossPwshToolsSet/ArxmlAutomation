608d701a7a72a206d0f6630ef32bfdf5173d2f5e
Hansong Li <li-hans@outlook.com>
2022-08-01 11:45:16 +0800

    add submit script and gallery

--- PSActions/PSGalleryPublish.ps1
+++ PSActions/PSGalleryPublish.ps1
@@ -1,2 +1,13 @@
+param(
+    [string]
+    $NugetKey
+)
 $PSVersionTable
-dir env:
+if($env:GITHUB_REF_NAME -eq "main"){
+    # main branch methods
+    Publish-Module -Path "$PSScriptRoot/../PSModules" -NuGetApiKey $NugetKey -Verbose -Confirm
+}
+else {
+    # sub branch methods
+    Publish-Module -Path "$PSScriptRoot/../PSModules" -NuGetApiKey $NugetKey -WhatIf -Verbose -Confirm
+}
