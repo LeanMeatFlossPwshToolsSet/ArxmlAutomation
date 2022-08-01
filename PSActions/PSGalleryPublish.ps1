5530eeb360bf2727dc1af16f446536c4629ddf13
Hansong Li <li-hans@outlook.com>
2022-08-01 12:11:03 +0800

    list child-item in root

--- PSActions/PSGalleryPublish.ps1
+++ PSActions/PSGalleryPublish.ps1
@@ -5,6 +5,7 @@ param(
 $PSVersionTable
 dir env:
 Set-PSRepository PSGallery -InstallationPolicy Trusted
+Get-ChildItem -Path "$($env:GITHUB_WORKSPACE)/PSModulesManifest"
 if($env:GITHUB_REF_NAME -eq "main"){
     # main branch methods
     Publish-Module -Path "$($env:GITHUB_WORKSPACE)/PSModulesManifest" -NuGetApiKey $NugetKey -Verbose -Confirm
