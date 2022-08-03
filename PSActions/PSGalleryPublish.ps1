bb491bc36dd0195409b14cc04b14d470e1cd71b2
Hansong Li <li-hans@outlook.com>
2022-08-03 17:21:16 +0800

    add new functions to submit

--- PSActions/PSGalleryPublish.ps1
+++ PSActions/PSGalleryPublish.ps1
@@ -1,18 +1,38 @@
 param(
     [string]
-    $NugetKey
+    $NugetKey,
+    [string]
+    $GitHubKey
 )
 $PSVersionTable
 dir env:
 Set-PSRepository PSGallery -InstallationPolicy Trusted
+$taggedVersion=git describe --match "v([0-9]\.){3}"
+if($LASTEXITCODE -ne 0){
+    $taggedVersion="v0.0.1"
+    Write-Host "Using $taggedVersion as the init version."
+}
+$taggedVersionArray=$taggedVersion.Split([string[]]@(".","v"),[System.StringSplitOptions]::RemoveEmptyEntries)
+$taggedVersionArray[-1]=([int]$taggedVersionArray[-1]+1).ToString()
+$newTagVersion
+$env:SubmitVersion=$newTagVersion -join "."
+$GitNewTaggedVersion="v$($env:SubmitVersion)"
+
+# increasing the version
+$rev=git rev-parse HEAD
+Write-Host "
+Current Commit $rev
+New Version need to be tagged $GitNewTaggedVersion
+"
 Get-ChildItem -Path "$($env:GITHUB_WORKSPACE)/ArxmlAutomation" -Directory |ForEach-Object{
     if($env:GITHUB_REF_NAME -eq "main"){
         # main branch methods
-        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -Verbose -Confirm -Force
+        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -Verbose -Force
+        
     }
     else {
         # sub branch methods
-        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -WhatIf -Verbose -Confirm -Force
+        Publish-Module -Path "$($_.FullName)" -NuGetApiKey $NugetKey -WhatIf -Verbose -Force
     }
 }
 
