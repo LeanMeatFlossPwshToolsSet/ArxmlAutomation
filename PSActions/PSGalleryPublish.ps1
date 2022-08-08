81839bd41c53552170ca6e9033e3462a3f1e7f16
Hansong Li <li-hans@outlook.com>
2022-08-08 13:51:58 +0800

    add write host for module on cloud

--- PSActions/PSGalleryPublish.ps1
+++ PSActions/PSGalleryPublish.ps1
@@ -47,6 +47,7 @@ New Version need to be tagged $GitNewTaggedVersion
 Get-ChildItem -Path "$($env:GITHUB_WORKSPACE)/ArxmlAutomation" -Directory |ForEach-Object{
     
     $moduleOnCloud=Find-Module -Name $_.Name
+    $moduleOnCloud|Write-Host
     if($moduleOnCloud){
         $cloudVersion=$moduleOnCloud.Version.Split([string[]]@(".","v"),[System.StringSplitOptions]::RemoveEmptyEntries)
         for ($i = 0; $i -lt $cloudVersion.Count; $i++) {
